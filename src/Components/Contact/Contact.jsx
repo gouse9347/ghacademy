37b02476b509026f41b69e1ec18795a66a725f07
Gouse9347 <gousemahammad.bingibai@gmail.com>
2025-07-17 18:33:25 +0530

    Initial commit

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -41,7 +41,7 @@ const Contact = () => {
         <h3>Send us a message <img src={msg_icon} alt="" /></h3>
         <p>Feel free to reach out through contact form or find our contact information below. Your feedback, questions, and suggestions are important to us as we strive to provide exceptional service to our university community.</p>
         <ul>
-            <li><img src={mail_icon} alt="" />Contact Gouse@GH Academy</li>
+            <li><img src={mail_icon} alt="" />Contact Gouse@ghacademy.com</li>
             <li><img src={phone_icon} alt="" />+91 9347238586</li>
             <li><img src={location_icon} alt="" />Nandyal, Andhra Pradesh<br/> India</li>
         </ul>
